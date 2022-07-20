b216d554948b3fe7d51a29fd532050b640d319c9
Anwesha-Bhattacharya <anweshabhattacharya12@gmail.com>
2022-07-20 21:18:04 +0530

    added functionality to play correct audio for each word/phrase

--- app/src/main/java/com/anwesha/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/anwesha/android/miwok/FamilyMembersActivity.java
@@ -1,7 +1,11 @@
 package com.anwesha.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,8 @@ import com.anwesha.android.miwok.R;
 import java.util.ArrayList;
 
 public class FamilyMembersActivity extends AppCompatActivity {
+    /** Handles playback of all the sound files */
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +23,18 @@ public class FamilyMembersActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         //List of English words for family members
-        ArrayList<Word> words = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("mother", "әṭa",R.drawable.family_mother));
-        words.add(new Word("father", "әpә",R.drawable.family_father));
-        words.add(new Word("daughter", "tune",R.drawable.family_daughter));
-        words.add(new Word("son", "angsi",R.drawable.family_son));
-        words.add(new Word("older sister", "teṭe",R.drawable.family_older_sister));
-        words.add(new Word("older brother", "taachi",R.drawable.family_older_brother));
-        words.add(new Word("younger sister", "kolliti",R.drawable.family_younger_sister));
-        words.add(new Word("younger brother", "chalitti",R.drawable.family_younger_brother));
-        words.add(new Word("grandmother", "ama",R.drawable.family_grandmother));
-        words.add(new Word("grandfather", "paapa",R.drawable.family_grandfather));
+        words.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        words.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        words.add(new Word("older sister", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("younger sister", "kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        words.add(new Word("grandmother ", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        words.add(new Word("grandfather", "paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s.
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
@@ -40,5 +46,22 @@ public class FamilyMembersActivity extends AppCompatActivity {
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
+
+
+        // Set a click listener to play the audio when the list item is clicked on
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Get the {@link Word} object at the given position the user clicked on
+                Word word = words.get(position);
+
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+                mMediaPlayer = MediaPlayer.create(FamilyMembersActivity.this, word.getAudioResourceID());
+
+                // Start the audio file
+                mMediaPlayer.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/anwesha/android/miwok/Word.java
+++ app/src/main/java/com/anwesha/android/miwok/Word.java
@@ -6,31 +6,46 @@ package com.anwesha.android.miwok;
  */
 public class Word {
 
-    //Default translation for the word
+    // Default translation for the word
     private String mDefaultTranslation;
-    //Miwok translation for the word
+    // Miwok translation for the word
     private String mMiwokTranslation;
-    //Image for the visual translation of the word
+    // Image Resource ID for the visual translation of the word
     private int mImageResourceID = NO_IMAGE_PROVIDED;
-
+    // Constant Value that represents no image was provided for this word
     private static final int NO_IMAGE_PROVIDED = -1;
+    // Audio Resource ID for the word
+    private int mAudioResourceID;
 
     /**
      * Create a new Word object.
      *
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
-     * @param miwokTranslation   is the word in the Miwok language
+     * @param miwokTranslation is the word in the Miwok language
+     * @param audioResourceId is the resource ID for the audio file associated with this word
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceID = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     * @param imageResourceId is the drawable resource ID for the image associated with the word
+     * @param audioResourceId is the resource ID for the audio file associated with this word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,
+                int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
-        mImageResourceID = imageResourceID;
+        mImageResourceID = imageResourceId;
+        mAudioResourceID = audioResourceId;
     }
 
     /**
@@ -47,17 +62,25 @@ public class Word {
         return mMiwokTranslation;
     }
 
+
     /**
-     * Get the visual translation of the word.
+     * Return the image resource ID of the word.
      */
     public int getImageResourceID() {
         return mImageResourceID;
     }
 
     /**
-     * Returns whether or not there is an image for this word
+     * Returns whether or not there is an image for this word.
      */
     public boolean hasImage() {
         return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceID() {
+        return mAudioResourceID;
     }
 }
